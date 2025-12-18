3823e121579f3b5d7435a5e011f73b2cd6f05629
dmsqja <bum17822@naver.com>
2025-12-18 12:25:34 +0900

    feat: Chapter 2 Prompt Template React UI 구현
    - PromptTemplate, ListOutput, MapOutput, BeanOutput, ParameterizedTypeReference 컴포넌트 추가
    - 지역/구분/언어 선택 드롭다운 UI 구현
    - 기존 Spring Boot 디자인과 동일한 Bootstrap 스타일 적용
    - /ch2/1~5_* 경로를 새 React 페이지와 라우팅으로 연결
    - 백엔드 API 미구현 상태에서는 더미 응답 텍스트만 표시 (추후 연동 예정)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,11 @@ import ChatStream from './pages/ch1/ChatStream';
 import ChatFewShot from './pages/ch1/ChatFewShot';
 import ChatChainOfThought from './pages/ch1/ChatChainOfThought';
 import ChatMemory from './pages/ch1/ChatMemory';
+import PromptTemplate from './pages/ch2/PromptTemplate';
+import ListOutput from './pages/ch2/ListOutput';
+import MapOutput from './pages/ch2/MapOutput';
+import BeanOutput from './pages/ch2/BeanOutput';
+import ParameterizedTypeReference from './pages/ch2/ParameterizedTypeReference';
 import './App.css';
 
 function App() {
@@ -24,6 +29,12 @@ function App() {
             <Route path="3_chat_few_shot" element={<ChatFewShot />} />
             <Route path="4_chat_chain_of_thought" element={<ChatChainOfThought />} />
             <Route path="5_chat_memory" element={<ChatMemory />} />
+            {/* Chapter 2 */}
+            <Route path="1_prompt_template" element={<PromptTemplate />} />
+            <Route path="2_list_output" element={<ListOutput />} />
+            <Route path="3_map_output" element={<MapOutput />} />
+            <Route path="4_bean_output" element={<BeanOutput />} />
+            <Route path="5_parameterized_type_reference" element={<ParameterizedTypeReference />} />
             {/* 다른 챕터의 세부 페이지는 여기에 추가 */}
           </Route>
         </Route>
